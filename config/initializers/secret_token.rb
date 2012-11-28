0efceee613439268816168d5d837056dfdbaf3fa
Marcos Hack <marcoshack@gmail.com>
2012-11-28 18:46:01 -0200

    step 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestApiExampleRailsApi::Application.config.secret_token = '084b86b1b6fbae6906d356f992e20c84db3c185becee3145391d879363e8ed9a780c7ebdd78c381ec160029839dada3b6c15237dee68cb8b172dd6fe51c726ac'
